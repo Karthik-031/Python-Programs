51475cdbedf32b99db4bfc89e18f4e11888dd024
Karthik-031 <karthikram7901@gmail.com>
2025-03-26 13:01:54 +0530

    Add files via upload

---
+++ if else two num.s
@@ -0,0 +1,13 @@
+n1=int(input("Enter first number:"))
+
+n2=int(input("Enter second number:"))
+
+if n1<n2:
+
+    sum=n1+n2
+
+    print("The sum is:",sum)
+
+else:
+        
+    print("Sub is:",sub)
---
+++ while loop natural no.s
@@ -0,0 +1,15 @@
+n=int(input("Enter a number:"))
+
+sum=0
+
+i=1
+
+while i<=n:
+
+    print(i)
+
+    sum=sum+1
+
+    i=i+1
+
+    print("The sum is:",sum)
